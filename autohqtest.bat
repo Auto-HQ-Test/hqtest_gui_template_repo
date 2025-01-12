a7e2326a0508c5a0edfdc06bcd4a70515c9d942b
natelynate <nately2nate@gmail.com>
2025-01-12 20:42:43 +0900

    Changed repo url

--- autohqtest.bat
+++ autohqtest.bat
@@ -2,7 +2,7 @@
 setlocal EnableDelayedExpansion
 
 :: Set the project repository URL and branch
-set REPO_URL=https://github.com/Auto-HQ-Test/autohqtest_gui
+set REPO_URL=https://github.com/Auto-HQ-Test/hqtest_gui_template_repo
 set BRANCH=main
 set INSTALL_DIR=%USERPROFILE%\auto_hqtest
 set PYTHON_VERSION=3.11.0
